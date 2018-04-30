3f7fded6014a4c8075e1ecb7397ed64cf058d7ff
Sam Spendla <samuelspendla@gmail.com>
2018-04-30 13:44:09 +0100

    Structured repo

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mysqlpw='SvW0iJ1QebUMjDDj'
-mysql -u sam -p"$mysqlpw" wca_stats < concise_results.sql
-mysql -u sam -p"$mysqlpw" wca_stats < PBs.sql
-mysql -u sam -p"$mysqlpw" wca_stats < PB_Streak.sql
-
-
