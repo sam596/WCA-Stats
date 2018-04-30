2dadcc2cc68563fd195bf646222d112ddc8a87f1
Sam Spendla <samuelspendla@gmail.com>
2018-05-01 00:46:45 +0100

    tables folder instead of queries and protected mysql password

--- bin/wcadevupd.sh
+++ bin/wcadevupd.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-mysqlpw='SvW0iJ1QebUMjDDj'
+source ~/mysqlpw/mysql.conf
 
 wget -O ~/databasedownload/wca-developer-database-dump.zip https://www.worldcubeassociation.org/wst/wca-developer-database-dump.zip
 unzip -o ~/databasedownload/wca-developer-database-dump.zip -d ~/databasedownload
 mysql -u sam -p"$mysqlpw" wca_stats -e "INSERT INTO wca_stats.last_updated VALUES ('wca_dev', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;"
 mysql -u sam -p"$mysqlpw" wca_dev < ~/databasedownload/wca-developer-database-dump.sql
 mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW() WHERE query = 'wca_dev'"
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/result_dates.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/all_single_results.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/SoR_Average.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/SoR_Single.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/world_ranks_all.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/SoR_Combined.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/persons_extra.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/records.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/concise_results.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/PBs.sql
-mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/PB_Streak.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/result_dates.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/all_single_results.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/SoR_Average.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/SoR_Single.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/world_ranks_all.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/SoR_Combined.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/persons_extra.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/records.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/concise_results.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/PBs.sql
+mysql -u sam -p"$mysqlpw" wca_stats < ~/sql/tables/PB_Streak.sql
