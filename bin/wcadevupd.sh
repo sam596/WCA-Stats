930bed95107a0e28e6adea4fcda2658901a0152b
Sam Spendla <samuelspendla@gmail.com>
2018-04-30 13:53:25 +0100

    fixed download directories

--- bin/wcadevupd.sh
+++ bin/wcadevupd.sh
@@ -2,10 +2,10 @@
 
 mysqlpw='SvW0iJ1QebUMjDDj'
 
-wget -O ~/wca-developer-database-dump.zip https://www.worldcubeassociation.org/wst/wca-developer-database-dump.zip
-unzip -o ~/wca-developer-database-dump.zip
+wget -O ~/databasedownload/wca-developer-database-dump.zip https://www.worldcubeassociation.org/wst/wca-developer-database-dump.zip
+unzip -o ~/databasedownload/wca-developer-database-dump.zip -d ~/databasedownload
 mysql -u sam -p"$mysqlpw" wca_stats -e "INSERT INTO wca_stats.last_updated VALUES ('wca_dev', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;"
-mysql -u sam -p"$mysqlpw" wca_dev < ~/wca-developer-database-dump.sql
+mysql -u sam -p"$mysqlpw" wca_dev < ~/databasedownload/wca-developer-database-dump.sql
 mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW() WHERE query = 'wca_dev'"
 mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/result_dates.sql
 mysql -u sam -p"$mysqlpw" wca_stats < ~/SQL/queries/all_single_results.sql
