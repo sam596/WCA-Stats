9e57411379f3a833b3d7225771a3b7ecd0a5d22e
Sam Spendla <samuelspendla@gmail.com>
2018-05-01 01:19:22 +0100

    kinch table edited and added to wcadevupd.sh

--- tables/kinch.sql
+++ tables/kinch.sql
@@ -2,30 +2,16 @@ INSERT INTO wca_stats.last_updated VALUES ('kinch', NOW(), NULL, '') ON DUPLICAT
 
 DROP TABLE IF EXISTS kinch_ranks_by_event;
 CREATE TABLE kinch_ranks_by_event
-    SELECT personId, eventId, countryId, continentId, MAX(worldkinch) worldkinch, MAX(continentkinch) continentkinch, MAX(countrykinch) countrykinch
-    FROM  (SELECT a.personId, a.countryId, z.continentId, a.eventId, b.format,
+    SELECT personId, name, eventId, countryId, continentId, MAX(worldkinch) worldkinch
+    FROM  (SELECT a.personId, a.name, a.countryId, z.continentId, a.eventId, b.format,
               (CASE WHEN b.best IS NULL 
               THEN 0 ELSE
                   (CASE WHEN a.eventId = '333mbf'
                   THEN (99-LEFT(b.best,2)+1-(MID(b.best,4,4)/3600))/(99-LEFT(c.best,2)+1-(MID(c.best,4,4)/3600))*100 ELSE
                   (c.best * 100 / b.best)
                   END)
-              END) AS worldkinch,
-              (CASE WHEN b.best IS NULL 
-              THEN 0 ELSE
-                  (CASE WHEN a.eventId = '333mbf'
-                  THEN (99-LEFT(b.best,2)+1-(MID(b.best,4,4)/3600))/(99-LEFT(d.best,2)+1-(MID(d.best,4,4)/3600))*100 ELSE
-                  (d.best * 100 / b.best)
-                  END)
-              END) AS continentkinch,           
-              (CASE WHEN b.best IS NULL 
-              THEN 0 ELSE
-                  (CASE WHEN a.eventId = '333mbf'
-                  THEN (99-LEFT(b.best,2)+1-(MID(b.best,4,4)/3600))/(99-LEFT(e.best,2)+1-(MID(e.best,4,4)/3600))*100 ELSE
-                  (e.best * 100 / b.best)
-                  END)
-              END) AS countrykinch
-          FROM (SELECT p.id personId, p.countryId, e.id eventId 
+              END) AS worldkinch
+          FROM (SELECT p.id personId, p.name, p.countryId, e.id eventId 
             FROM wca_dev.persons p
             JOIN wca_dev.events e
             WHERE e.rank < 900 AND p.subid = 1) a
@@ -51,29 +37,7 @@ CREATE TABLE kinch_ranks_by_event
               FROM wca_dev.rankssingle
               WHERE eventId IN ('333mbf','333fm','333bf','444bf','555bf')
                 AND worldrank = 1) c
-          ON a.eventID = c.eventId AND b.format = c.format
-          LEFT JOIN
-            (SELECT personId, eventId, best, 'a' format 
-            FROM wca_dev.ranksaverage
-              WHERE eventId NOT IN ('333mbf','444bf','555bf')
-                AND continentrank = 1
-              UNION ALL
-              SELECT personId, eventId, best, 's' format
-              FROM wca_dev.rankssingle
-              WHERE eventId IN ('333mbf','333fm','333bf','444bf','555bf')
-                AND continentrank = 1) d
-          ON a.eventID = c.eventId AND b.format = c.format AND (SELECT continentid FROM wca_dev.countries WHERE id = (SELECT countryId FROM wca_dev.persons WHERE subid = 1 AND id = d.personId)) = z.continentId
-          LEFT JOIN
-            (SELECT personId, eventId, best, 'a' format 
-            FROM wca_dev.ranksaverage
-              WHERE eventId NOT IN ('333mbf','444bf','555bf')
-                AND countryrank = 1
-              UNION ALL
-              SELECT personId, eventId, best, 's' format
-              FROM wca_dev.rankssingle
-              WHERE eventId IN ('333mbf','333fm','333bf','444bf','555bf')
-                AND countryrank = 1) e
-          ON a.eventID = c.eventId AND b.format = c.format AND (SELECT countryId FROM wca_dev.persons WHERE subid = 1 AND id = e.personId) = a.countryId) a
+          ON a.eventID = c.eventId AND b.format = c.format) a
     GROUP BY
         personId,
         eventId
@@ -81,7 +45,7 @@ CREATE TABLE kinch_ranks_by_event
 
 DROP TABLE IF EXISTS kinch;
 CREATE TABLE kinch
-    SELECT personId, countryId, continentId, ROUND(AVG(worldkinch),2) worldkinch, ROUND(AVG(continentkinch),2) continentkinch, ROUND(AVG(countrykinch),2) countrykinch
+    SELECT personId, name, countryId, continentId, ROUND(AVG(worldkinch),2) worldkinch
     FROM wca_stats.kinch_ranks_by_event
     GROUP BY personId
     ORDER BY worldkinch DESC
