d37d585339d0f0020c3d95b9c8c939dfa524b332
Sam Spendla <samuelspendla@gmail.com>
2018-05-17 00:04:17 +0100

    added delegate/organiser fields to persons_extra

--- tables/persons_extra.sql
+++ tables/persons_extra.sql
@@ -1,10 +1,9 @@
-
 INSERT INTO wca_stats.last_updated VALUES ('persons_extra', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;
 
 DROP TABLE IF EXISTS persons_extra;
 CREATE TABLE persons_extra	
 		SELECT
-			a.id, a.name, a.gender, a.countryId, b.continentId, c.competitions, c.eventsAttempted, c.eventsSucceeded, c.eventsAverage, c.finals, c.podiums, c.gold, c.silver, c.bronze, c.eventsPodiumed, c.eventsWon, c.wcPodium, c.wChampion, c.records, c.WRs, c.CRs, c.NRs, c.countries, c.continents, c.multipleCountryComps, c.distinctMultipleCountryComps, d.completedSolves, d.DNFs, e.SoR_average `sorAverage`, e.SoR_average_rank `sorAverageRank`, e.SoR_single `sorSingle`, e.SoR_single_rank `sorSingleRank`, e.Combined_SoR `sorCombined`, e.rank `sorCombinedRank`, f.minWorldRank, (CASE WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 AND c.WRs > 0 AND c.CRs > 0 AND c.wcPodium > 0 THEN 'Platinum' WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 AND (c.WRs > 0 OR c.CRs > 0 OR c.wcPodium > 0) THEN 'Gold' WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 THEN 'Silver' WHEN c.eventsSucceeded = 18 THEN 'Bronze' ELSE NULL END) `membership`
+			a.id, g.id user_id, a.name, a.gender, a.countryId, b.continentId, c.competitions, c.eventsAttempted, c.eventsSucceeded, c.eventsAverage, c.finals, c.podiums, c.gold, c.silver, c.bronze, c.eventsPodiumed, c.eventsWon, c.wcPodium, c.wChampion, c.records, c.WRs, c.CRs, c.NRs, c.countries, c.continents, c.multipleCountryComps, c.distinctMultipleCountryComps, d.completedSolves, d.DNFs, e.SoR_average `sorAverage`, e.SoR_average_rank `sorAverageRank`, e.SoR_single `sorSingle`, e.SoR_single_rank `sorSingleRank`, e.Combined_SoR `sorCombined`, e.rank `sorCombinedRank`, f.minWorldRank, (CASE WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 AND c.WRs > 0 AND c.CRs > 0 AND c.wcPodium > 0 THEN 'Platinum' WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 AND (c.WRs > 0 OR c.CRs > 0 OR c.wcPodium > 0) THEN 'Gold' WHEN c.eventsSucceeded = 18 AND c.eventsAverage = 15 THEN 'Silver' WHEN c.eventsSucceeded = 18 THEN 'Bronze' ELSE NULL END) `membership`, g.delegate_status delegateStatus, g.region, g.location_description location, IF(h.competitionsDelegated IS NULL AND g.delegate_status IS NOT NULL, 0, h.competitionsDelegated) competitionsDelegated, IFNULL(j.competitionsOrganized,0) competitionsOrganized, k.wcaTeam
 		FROM (SELECT * FROM wca_dev.persons WHERE subid = 1) a
 		LEFT JOIN wca_dev.Countries b
 			ON a.countryId = b.id
@@ -50,6 +49,19 @@ CREATE TABLE persons_extra
 			FROM world_ranks_all
 			WHERE competed = 1
 			GROUP BY personId) f
-		ON a.id = f.personId;
+		ON a.id = f.personId
+		LEFT JOIN
+			wca_dev.users g
+		ON a.id = g.wca_id
+		LEFT JOIN
+			(SELECT delegate_id, COUNT(DISTINCT competition_id) competitionsDelegated FROM wca_dev.competition_delegates GROUP BY delegate_id) h
+		ON g.id = h.delegate_id
+		LEFT JOIN
+			(SELECT organizer_id, COUNT(DISTINCT competition_id) competitionsOrganized FROM wca_dev.competition_organizers GROUP BY organizer_id) j
+		ON g.id = j.organizer_id
+		LEFT JOIN
+			(SELECT tm.user_id, GROUP_CONCAT(t.friendly_id ORDER BY t.id ASC) wcaTeam FROM wca_dev.team_members tm JOIN wca_dev.teams t ON tm.team_id = t.id GROUP BY tm.user_id) k 
+		ON g.id = k.user_id
+		;
 
 UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'persons_extra';
