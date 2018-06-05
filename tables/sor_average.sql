24971d6b54bd955fbb31c0f9635929d94c687b28
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-06-05 11:13:00 +0100

    Renames for consistency (#11)
    * Rename PB_Streak.sql to pb_streak.sql
    
    * Rename PBs.sql to pbs.sql
    
    * Rename SoR_Average.sql to sor_average.sql
    
    * Rename SoR_Combined.sql to sor_combined.sql
    
    * Rename SoR_Single.sql to sor_single.sql
    
    * Update readme.md
    
    * Create relays.sql
    
    * Update sor_combined.sql
    
    * Delete world_ranks_all.sql
    
    * Update readme.md
    
    * Rename PBTime.sql to pbtime.sql
    
    * Rename SQL.md to sql.md
    
    * Update wcadevstsupd.sh
    
    * Update sor_combined.sql
    
    * Update sor_average.sql
    
    * Update sor_combined.sql
    
    * Update sor_single.sql
    
    * Update pbs.sql
    
    * Update pb_streak.sql

--- tables/sor_average.sql
+++ tables/sor_average.sql
@@ -1,10 +1,10 @@
-INSERT INTO wca_stats.last_updated VALUES ('SoR_single', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;
+INSERT INTO wca_stats.last_updated VALUES ('sor_average', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;
 
-DROP TABLE IF EXISTS SoR_single_be1;
-CREATE TEMPORARY TABLE SoR_single_be1 AS 
+DROP TABLE IF EXISTS SoR_average_be1;
+CREATE TEMPORARY TABLE SoR_average_be1 AS 
 	SELECT 
-		p.id personId, 
-		p.name name,
+		p.id personId,
+		p.name name, 
 		e.id eventId 
 	FROM 
 		wca_dev.persons p 
@@ -13,56 +13,54 @@ CREATE TEMPORARY TABLE SoR_single_be1 AS
 		FROM 
 			wca_dev.events 
 		WHERE 
-			id NOT IN ('333mbo','magic','mmagic')
+			id NOT IN ('333mbo','magic','mmagic','444bf','555bf','333mbf')
 		) e
 ;
 
-
-DROP TABLE IF EXISTS world_single_ranks;
-CREATE TABLE world_single_ranks AS
+DROP TABLE IF EXISTS world_average_ranks;
+CREATE TABLE world_average_ranks AS
 	SELECT 
-		a.personId, 
-		a.name,
-		a.eventId,
+		a.personId,
+		a.name, 
+		a.eventId, 
 		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE c.best END) best,
 		(CASE WHEN c.worldrank IS NULL THEN b.count + 1 ELSE c.worldrank END) worldrank,
 		(CASE WHEN c.worldrank IS NULL THEN 0 ELSE 1 END) competed,
 		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE d.competitionId END) competitionId,
 		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE d.roundTypeId END) roundTypeId,
-		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE d.value END) result,
+		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE d.average END) result,
 		(CASE WHEN c.worldrank IS NULL THEN NULL ELSE d.date END) date
 	FROM 	
-		SoR_single_be1 a
-	JOIN
-		(SELECT 
-			eventId, 
+		SoR_average_be1 a
+	JOIN (
+		SELECT 
+			eventId eventId, 
 			COUNT(*) `count`
 		FROM 
-			wca_dev.rankssingle
+			wca_dev.ranksaverage
 		GROUP BY 
 			eventId
 		) b
 	ON 
 		a.eventId=b.eventId
 	LEFT JOIN	
-		wca_dev.rankssingle c
+		wca_dev.ranksaverage c
 	ON 
 		a.personId = c.personId 
 		AND 
-		a.eventId = c.eventId
-	LEFT JOIN 
-		all_single_results d
+		a.eventId = c.eventId		
+	LEFT JOIN
+		result_dates d
 	ON
 		a.personId = d.personId
 		AND
 		a.eventId = d.eventId
-		AND 
-		c.best = d.value	
+		AND
+		c.best = d.average
 ;
 
-
-DROP TABLE IF EXISTS SoR_single;
-CREATE TABLE SoR_single
+DROP TABLE IF EXISTS SoR_average;
+CREATE TABLE SoR_average 
 (rank INT NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY(rank))
 	SELECT 
@@ -70,11 +68,11 @@ PRIMARY KEY(rank))
 		name, 
 		SUM(worldrank) SoR 
 	FROM 
-		world_single_ranks 
+		world_average_ranks
 	GROUP BY 
 		personId 
-	ORDER BY 
+	ORDER BY
 		SoR ASC
 ;
 
-UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'SoR_single';
+UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'sor_average';
