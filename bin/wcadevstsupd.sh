639d72cd703aa7343574efaeaeb9796c466973a4
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-06-05 16:10:25 +0100

    Update wcadevstsupd.sh

--- bin/wcadevstsupd.sh
+++ bin/wcadevstsupd.sh
@@ -4,6 +4,7 @@ source ~/mysqlpw/mysql.conf
 
 wget -O ~/databasedownload/wca-developer-database-dump.zip https://www.worldcubeassociation.org/wst/wca-developer-database-dump.zip
 unzip -o ~/databasedownload/wca-developer-database-dump.zip -d ~/databasedownload
+mysql -u sam -p"$mysqlpw" wca_stats -e "INSERT INTO wca_stats.last_updated VALUES ('wcadevstsupd.sh', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;"
 mysql -u sam -p"$mysqlpw" wca_stats -e "INSERT INTO wca_stats.last_updated VALUES ('wca_dev', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;"
 mysql -u sam -p"$mysqlpw" wca_dev < ~/databasedownload/wca-developer-database-dump.sql
 mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW() WHERE query = 'wca_dev'"
@@ -20,3 +21,4 @@ mysql -u sam -p"$mysqlpw" wca_stats < ~/WCA-Stats/tables/pbs.sql
 mysql -u sam -p"$mysqlpw" wca_stats < ~/WCA-Stats/tables/pb_streak.sql
 mysql -u sam -p"$mysqlpw" wca_stats < ~/WCA-Stats/tables/mbld_decoded.sql
 mysql -u sam -p"$mysqlpw" wca_stats < ~/WCA-Stats/tables/relays.sql
+mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW() WHERE query = 'wcadevstsupd.sh'"
