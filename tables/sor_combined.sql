21a21eb9250d7c0ef44727fd8c1e0eb97be02df8
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-06-05 10:53:28 +0100

    Update sor_combined.sql

--- tables/sor_combined.sql
+++ tables/sor_combined.sql
@@ -23,3 +23,20 @@ PRIMARY KEY(rank))
 ;
 
 UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'SoR_Combined';
+
+INSERT INTO wca_stats.last_updated VALUES ('world_ranks_all', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;
+
+DROP TABLE IF EXISTS world_ranks_all;
+CREATE TABLE world_ranks_all AS
+	SELECT 	*,
+			'a' `format`
+	FROM
+		world_average_ranks
+	UNION ALL
+	SELECT	*,
+			's' `format`
+	FROM
+		world_single_ranks
+;
+
+UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'world_ranks_all';
