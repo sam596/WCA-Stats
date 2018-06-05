f9c64cc7360bdb47e39bf9febd0d68653ffb7557
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-06-05 10:41:33 +0100

    Update PB_Streak.sql

--- tables/PB_Streak.sql
+++ tables/PB_Streak.sql
@@ -14,4 +14,18 @@ SELECT a.*,
 FROM (SELECT * FROM competition_PBs ORDER BY id ASC) a
 GROUP BY a.personId, a.competitionId ORDER BY a.id ASC;
 
+SET @val = 0;
+SET @pid = NULL;
+SET @scomp = NULL;
+SET @ecomp = NULL;
+DROP TABLE IF EXISTS PB_Streak_exFMC;
+CREATE TABLE PB_Streak_exFMC
+SELECT a.*, 
+        @val := IF(a.PBs = 0, 0, IF(a.personId = @pid, @val + 1, 1)) pbStreak,
+        @scomp := IF(@val = 0, NULL, IF(@val = 1, competitionId, @scomp)) startComp,
+        @ecomp := IF(@val = 0, NULL, competitionId) endComp,
+        @pid := personId pidhelp
+FROM (SELECT * FROM competition_PBs_exFMC ORDER BY id ASC) a
+GROUP BY a.personId, a.competitionId ORDER BY a.id ASC;
+
 UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'PB_Streak';
