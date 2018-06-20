4ec38551ee40b114e30077a4f46b5cf1afaccf45
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-06-20 17:01:08 +0200

    Optimisation (#23)
    * Update persons_extra.sql
    
    * Update all_single_results.sql
    
    Rewrote indexes and removed unneeded ones.
    
    * Update championship_podium.sql
    
    * Update concise_results.sql
    
    * Update kinch.sql
    
    * Update kinch.sql
    
    * Update kinch.sql
    
    * Update kinch.sql
    
    * Update championship_podium.sql
    
    * Update mbld_decoded.sql
    
    * Update pb_streak.sql
    
    * Update pbs.sql
    
    * Update concise_results.sql
    
    * Update records.sql
    
    * Update sor_average.sql
    
    * Update sor_single.sql
    
    * Update sor_combined.sql
    
    * Update result_dates.sql

--- tables/records.sql
+++ tables/records.sql
@@ -2,11 +2,11 @@ INSERT INTO wca_stats.last_updated VALUES ('records', NOW(), NULL, '') ON DUPLIC
 
 DROP TABLE IF EXISTS records;
 CREATE TABLE records
-(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))
+(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), KEY pce (personId, competitionId, eventId), KEY rec (record))
 SELECT * FROM
-(SELECT competitionId, date, eventId, countryId, personId, average, best, 'a' record FROM result_dates WHERE regionalAverageRecord <> ''
+(SELECT competitionId, date, eventId, countryId, personId, average, best, 'a' format, regionalAverageRecord record FROM result_dates WHERE regionalAverageRecord <> ''
 UNION ALL 
-SELECT competitionId, date, eventId, countryId, personId, average, best, 's' record FROM result_dates WHERE regionalSingleRecord <> '') a
+SELECT competitionId, date, eventId, countryId, personId, average, best, 's' format, regionalSingleRecord record FROM result_dates WHERE regionalSingleRecord <> '') a
 ORDER BY countryId, eventId, record, date ASC;
 
 UPDATE wca_stats.last_updated SET completed = NOW() WHERE query = 'records';
