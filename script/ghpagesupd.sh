9e69d25cc4cc6d5630c480d062b157ae63e39d52
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-09-02 15:33:59 +0100

    Update ghpagesupd.sh

--- script/ghpagesupd.sh
+++ script/ghpagesupd.sh
@@ -10,7 +10,7 @@ declare -a arr=(5 6 7 8 9 10)
 for i in "${arr[@]}"
 do
 	echo "Best Average without Sub ${i} Single"
-	mysql -u sam -p"$mysqlpw" wca_dev -e "SELECT CONCAT('[',p.name,'](https://www.worldcubeassociation.org/persons/',a.personId,')') Name, p.countryId Country, (SELECT ROUND(best/100,2) FROM rankssingle WHERE eventId = '333' AND personId = a.personId) Single, ROUND(a.best/100,2) Average FROM ranksaverage a INNER JOIN persons p ON p.subid = 1 AND a.personId = p.id WHERE a.eventId = '333' AND personId NOT IN (SELECT personId FROM rankssingle WHERE eventId = '333' AND best < ${i}00) ORDER BY average ASC, Single ASC, personId LIMIT 250;" > ~/mysqloutput/original && \
+	mysql -u sam -p"$mysqlpw" wca_dev -e "SELECT CONCAT('[',p.name,'](https://www.worldcubeassociation.org/persons/',a.personId,')') Name, p.countryId Country, (SELECT ROUND(best/100,2) FROM rankssingle WHERE eventId = '333' AND personId = a.personId) Single, ROUND(a.best/100,2) Average FROM ranksaverage a INNER JOIN persons p ON p.subid = 1 AND a.personId = p.id WHERE a.eventId = '333' AND personId NOT IN (SELECT personId FROM rankssingle WHERE eventId = '333' AND best < ${i}00) ORDER BY average ASC LIMIT 250;" > ~/mysqloutput/original && \
 	sed 's/\t/|/g' ~/mysqloutput/original > ~/mysqloutput/output && \
 	sed -i.bak '2i\
 --|--|--|--\' ~/mysqloutput/output
@@ -42,7 +42,7 @@ JOIN wca_dev.persons p ON a.personId = p.id AND p.subid = 1 \
 JOIN wca_dev.competitions com ON a.competitionid = com.id \
 WHERE eventId = '${i}' \
 GROUP BY competitionId, eventId HAVING COUNT(*) = 3 \
-ORDER BY SUM(result), com.start_date ASC LIMIT 1000;" > ~/mysqloutput/original && \
+ORDER BY SUM(result) LIMIT 1000;" > ~/mysqloutput/original && \
 	sed 's/\t/|/g' ~/mysqloutput/original > ~/mysqloutput/output && \
     sed -i.bak '2i\
 --|--|--|--|--\' ~/mysqloutput/output
@@ -67,7 +67,7 @@ do
 	elif [ "$i" = "pb_streak_exfmcbld" ]; then text=$(echo "PB Streak excluding FMC-and-BLD-Only Comps")
 	fi
 	echo "Longest ${i}"
-	mysql -u sam -p"$mysqlpw" wca_stats -e "SELECT CONCAT('[',p.name,'](https://www.worldcubeassociation.org/persons/',a.personId,')') name, a.pbStreak \`PB Streak\`, CONCAT('[',a.startcomp,'](https://www.worldcubeassociation.org/competitions/',a.startcomp,')') \`Start Comp\`, IF((SELECT id FROM ${i} WHERE personId = a.personId AND endcomp = a.endComp)=(SELECT MAX(id) FROM ${i} WHERE personId = a.personId),'',CONCAT('[',(SELECT competitionId FROM ${i} WHERE id = a.id + 1),'](https://www.worldcubeassociation.org/competitions/',(SELECT competitionId FROM ${i} WHERE id = a.id + 1),')' )) \`End Comp\` FROM ${i} a INNER JOIN (SELECT personId, startcomp, MAX(pbStreak) maxpbs FROM ${i} GROUP BY personId, startcomp) b ON a.personId = b.personId and a.startcomp = b.startcomp and b.maxpbs = a.pbstreak JOIN wca_dev.persons p ON a.personId = p.id ORDER BY a.pbStreak DESC, a.personId LIMIT 1000;" > ~/mysqloutput/original && \
+	mysql -u sam -p"$mysqlpw" wca_stats -e "SELECT CONCAT('[',p.name,'](https://www.worldcubeassociation.org/persons/',a.personId,')') name, a.pbStreak \`PB Streak\`, CONCAT('[',a.startcomp,'](https://www.worldcubeassociation.org/competitions/',a.startcomp,')') \`Start Comp\`, IF((SELECT id FROM ${i} WHERE personId = a.personId AND endcomp = a.endComp)=(SELECT MAX(id) FROM ${i} WHERE personId = a.personId),'',CONCAT('[',(SELECT competitionId FROM ${i} WHERE id = a.id + 1),'](https://www.worldcubeassociation.org/competitions/',(SELECT competitionId FROM ${i} WHERE id = a.id + 1),')' )) \`End Comp\` FROM ${i} a INNER JOIN (SELECT personId, startcomp, MAX(pbStreak) maxpbs FROM ${i} GROUP BY personId, startcomp) b ON a.personId = b.personId and a.startcomp = b.startcomp and b.maxpbs = a.pbstreak JOIN wca_dev.persons p ON a.personId = p.id ORDER BY a.pbStreak DESC LIMIT 1000;" > ~/mysqloutput/original && \
 	sed 's/\t/|/g' ~/mysqloutput/original > ~/mysqloutput/output && \
 	sed -i.bak '2i\
 --|--|--|--\' ~/mysqloutput/output
@@ -81,53 +81,25 @@ do
 	rm ~/pages/WCA-Stats/pbstreaks/*.tmp*
 done
 
-#mostsubxsinglewithoutsubxaverage
+#registrationslist
 
-declare -a arr=(6 7 8 9 10)
+declare -a arr=(name competitionId)
 
 for i in "${arr[@]}"
 do
-	echo "Most Sub-${i} Singles without a Sub-${i} Average"
-	mysql -u sam -p"$mysqlpw" wca_stats -e "SELECT CONCAT('[',personname,'](https://www.worldcubeassociation.org/persons/',personId,')') Name, COUNT(*) \`Sub-${i}s\`, (SELECT ROUND(best/100,2) FROM wca_dev.ranksaverage WHERE personId = a.personId AND eventId = '333') Average FROM wca_stats.all_single_results a WHERE value > 0 AND value < ${i}00 AND eventId = '333' AND personId NOT IN (SELECT personId FROM wca_dev.ranksaverage WHERE eventId = '333' AND best < ${i}00) GROUP BY personId ORDER BY COUNT(*) DESC, Average LIMIT 250;" > ~/mysqloutput/original && \
+	echo "Registration List ordered by ${i}"
+	mysql -u sam -p"$mysqlpw" wca_stats -e "SELECT   IF(a.personId IS NULL,a.name,CONCAT('[',a.name,'](https://www.worldcubeassociation.org/persons/',a.personId,')')) Name,   p.countryId Country,   CONCAT('[',a.competitionId,'](https://www.worldcubeassociation.org/competitions/',a.competitionId,')') Competition,    (CASE   WHEN acceptedAt IS NULL AND deletedAt IS NULL THEN 'Pending'  WHEN acceptedAt IS NOT NULL AND deletedAt IS NULL THEN 'Accepted'  WHEN deletedAt IS NOT NULL THEN 'Deleted'  ELSE 'error'END) Registration_Status,  CONCAT(    CASE WHEN a.333 = 1 THEN '333,' ELSE '' END,    CASE WHEN a.222 = 1 THEN '222,' ELSE '' END,    CASE WHEN a.444 = 1 THEN '444,' ELSE '' END,    CASE WHEN a.555 = 1 THEN '555,' ELSE '' END,    CASE WHEN a.666 = 1 THEN '666,' ELSE '' END,    CASE WHEN a.777 = 1 THEN '777,' ELSE '' END,    CASE WHEN a.333bf = 1 THEN '333bf,' ELSE '' END,    CASE WHEN a.333fm = 1 THEN '333fm,' ELSE '' END,    CASE WHEN a.333ft = 1 THEN '333ft,' ELSE '' END,    CASE WHEN a.333oh = 1 THEN '333oh,' ELSE '' END,    CASE WHEN a.clock = 1 THEN 'clock,' ELSE '' END,    CASE WHEN a.minx = 1 THEN 'minx,' ELSE '' END,    CASE WHEN a.pyram = 1 THEN 'pyram,' ELSE '' END,    CASE WHEN a.skewb = 1 THEN 'skewb,' ELSE '' END,    CASE WHEN a.sq1 = 1 THEN 'sq1,' ELSE '' END,    CASE WHEN a.444bf = 1 THEN '444bf,' ELSE '' END,    CASE WHEN a.555bf = 1 THEN '555bf,' ELSE '' END,    CASE WHEN a.333mbf = 1 THEN '333mbf,' ELSE '' END) Events FROM registrations_extra a LEFT JOIN wca_dev.persons p ON a.personId = p.id WHERE a.endDate >= CURDATE() ORDER BY ${i}, a.name, a.startDate, a.endDate;"> ~/mysqloutput/original && \
 	sed 's/\t/|/g' ~/mysqloutput/original > ~/mysqloutput/output && \
 	sed -i.bak '2i\
---|--|--\' ~/mysqloutput/output
+--|--|--|--|--\' ~/mysqloutput/output
 	sed -i.bak 's/^/|/' ~/mysqloutput/output
 	sed -i.bak 's/$/|  /' ~/mysqloutput/output
 	date=$(date -r ~/databasedownload/wca-developer-database-dump.zip +"%a %b %d at %H%MUTC")
-	cp ~/pages/WCA-Stats/templates/mostsubxsinglewithoutsubxaverage.md ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md.tmp
-	cat ~/mysqloutput/output >> ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md.tmp
-    awk -v r="$i" '{gsub(/xxx/,r)}1' ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md.tmp > ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md.tmp2 && \
-	awk -v r="$date" '{gsub(/today_date/,r)}1' ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md.tmp2 > ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/$i.md && \
-	rm ~/pages/WCA-Stats/mostsubxsinglewithoutsubxaverage/*.tmp*
-done
-
-#sumofbesttimesatcompetition
-
-declare -a arr=(all ex45bf)
-
-for i in "${arr[@]}"
-do
-	echo "Sum of times at competition ${i}"
-	if [ "$i" = "all" ]; 
-		then 
-			text=$(echo "All competitions excluding MBLD and FMC")
-			mysql -u sam -p"$mysqlpw" wca_dev -e "SELECT CONCAT('[',competitionId,'](https://www.worldcubeassociation.org/competitions/',competitionId,')') Competition, (SELECT countryId FROM competitions WHERE id = a.competitionId) Country, LEFT(TIME_FORMAT(SEC_TO_TIME(SUM(best)/100),'%i:%s.%f'),8) \`Sum\` FROM (SELECT competitionId, eventId, MIN(best) best FROM results WHERE competitionId IN (SELECT competitionId FROM results WHERE eventId IN ('333','222','444','555','666','777','333oh','333bf','333ft','clock','skewb','pyram','minx','sq1','444bf','555bf') AND best > 0 GROUP BY competitionId HAVING COUNT(DISTINCT eventId) = 16) AND best > 0 AND eventId NOT IN ('333mbf','333fm') GROUP BY competitionId, eventId) a GROUP BY competitionId ORDER BY SUM(best) ASC, competitionId LIMIT 500;" > ~/mysqloutput/original
-		else 
-			text=$(echo "All competitions excluding MBLD, FMC, 4BLD and 5BLD")
-			mysql -u sam -p"$mysqlpw" wca_dev -e "SELECT CONCAT('[',competitionId,'](https://www.worldcubeassociation.org/competitions/',competitionId,')') Competition, (SELECT countryId FROM competitions WHERE id = a.competitionId) Country, LEFT(TIME_FORMAT(SEC_TO_TIME(SUM(best)/100),'%i:%s.%f'),8) \`Sum\` FROM (SELECT competitionId, eventId, MIN(best) best FROM results WHERE competitionId IN (SELECT competitionId FROM results WHERE eventId IN ('333','222','444','555','666','777','333oh','333bf','333ft','clock','skewb','pyram','minx','sq1') AND best > 0 GROUP BY competitionId HAVING COUNT(DISTINCT eventId) = 14) AND best > 0 AND eventId NOT IN ('333mbf','333fm','444bf','555bf') GROUP BY competitionId, eventId) a GROUP BY competitionId ORDER BY SUM(best) ASC LIMIT 500;" > ~/mysqloutput/original
-	fi
-	sed 's/\t/|/g' ~/mysqloutput/original > ~/mysqloutput/output
-	sed -i.bak '2i\
---|--|--\' ~/mysqloutput/output
-	sed -i.bak 's/^/|/' ~/mysqloutput/output
-	sed -i.bak 's/$/|  /' ~/mysqloutput/output
-	date=$(date -r ~/databasedownload/wca-developer-database-dump.zip +"%a %b %d at %H%MUTC")
-	cp ~/pages/WCA-Stats/templates/sumbesttime.md ~/pages/WCA-Stats/sumbesttime/$i.md.tmp
-	cat ~/mysqloutput/output >> ~/pages/WCA-Stats/sumbesttime/$i.md.tmp
-	awk -v r="$text" '{gsub(/xxx/,r)}1' ~/pages/WCA-Stats/sumbesttime/$i.md.tmp > ~/pages/WCA-Stats/sumbesttime/$i.md.tmp2
-	awk -v r="$date" '{gsub(/today_date/,r)}1' ~/pages/WCA-Stats/sumbesttime/$i.md.tmp2 > ~/pages/WCA-Stats/sumbesttime/$i.md
-	rm ~/pages/WCA-Stats/sumbesttime/*.tmp*
+    cp ~/pages/WCA-Stats/templates/registrations.md ~/pages/WCA-Stats/registrations/$i.md.tmp
+    cat ~/mysqloutput/output >> ~/pages/WCA-Stats/registrations/$i.md.tmp
+    awk -v r="$i" '{gsub(/xxx/,r)}1' ~/pages/WCA-Stats/registrations/$i.md.tmp > ~/pages/WCA-Stats/registrations/$i.md.tmp2 && \
+	awk -v r="$date" '{gsub(/today_date/,r)}1' ~/pages/WCA-Stats/registrations/$i.md.tmp2 > ~/pages/WCA-Stats/registrations/$i.md && \
+	rm ~/pages/WCA-Stats/registrations/*.tmp*
 done
 
 d=$(date +%Y-%m-%d)
