1d4d60cedfe8b5763927c17537c6b74d4e41fc5b
Sam Spendla <31994452+sam596@users.noreply.github.com>
2018-11-10 02:46:24 +0000

    Update records.sql
    Fixed continentId > personContinentId

--- tables/records.sql
+++ tables/records.sql
@@ -3,11 +3,11 @@ INSERT INTO wca_stats.last_updated VALUES ('records', NOW(), NULL, '') ON DUPLIC
 DROP TABLE IF EXISTS National_Records;
 CREATE TABLE National_Records
 (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalSingleRecord != ''
 UNION ALL
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalAverageRecord != ''
 ORDER BY personCountryId, eventId, format DESC, date ASC;
@@ -17,25 +17,25 @@ ORDER BY personCountryId, eventId, format DESC, date ASC;
 DROP TABLE IF EXISTS Continent_Records;
 CREATE TABLE Continent_Records
 (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalSingleRecord NOT IN ('','NR')
 UNION ALL
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalAverageRecord NOT IN ('','NR')
-ORDER BY continentId, eventId, format DESC, date ASC;
+ORDER BY personContinentId, eventId, format DESC, date ASC;
 
 # <10 secs
 
 DROP TABLE IF EXISTS World_Records;
 CREATE TABLE World_Records
 (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 's' format, best 'result', regionalSingleRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalSingleRecord = 'WR'
 UNION ALL
-SELECT competitionId, date, personId, personName, personCountryId, continentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
+SELECT competitionId, date, personId, personName, personCountryId, personContinentId, eventId, 'a' format, average 'result', regionalAverageRecord 'record'
 	FROM wca_stats.results_extra
 	WHERE regionalAverageRecord = 'WR'
 ORDER BY eventId, format DESC, date ASC;
