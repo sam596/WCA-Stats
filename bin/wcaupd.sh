d9c5022259d70e76008cffe20d4c4a29cfecbe18
sam596 <31994452+sam596@users.noreply.github.com>
2020-03-05 16:48:02 +0000

    script files update
    Adds comments to all files, adds timers to the discord webhooks, uses a script to send webhooks instead of curl command, updates readme to reflect this.

--- bin/wcaupd.sh
+++ bin/wcaupd.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+# link to mysql password
 source ~/.mysqlpw/mysql.conf
 
+# define files
 dbURL="https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip"
 dbLocal=~/databasedownload/WCA_export.sql.zip
 dbPath=~/databasedownload
 
-curl -H "Content-Type: application/json" -X POST -d '{"username": "WCA-Stats", "content": "The latest **public** WCA export is now being imported to `wca`"}' $discordwh
-  
+# tell discord
+~/.mysqlpw/discord-notify.sh "The latest **public** WCA export is now being imported to \`wca\`"
+
+# download database
 curl -sRo "${dbLocal}" "${dbURL}"
-  
+
+# unzip database
 unzip -o "${dbLocal}" -d "${dbPath}" -x "README.txt"
 
+# log starting
 mysql -u sam -p"$mysqlpw" wca_stats -e "INSERT INTO wca_stats.last_updated VALUES ('wca', NOW(), NULL, '') ON DUPLICATE KEY UPDATE started=NOW(), completed = NULL;"
 
+# import, tell discord and log finish
 mysql -u sam -p"$mysqlpw" wca < ~/databasedownload/WCA_export.sql && \
-curl -H "Content-Type: application/json" -X POST -d '{"username": "WCA-Stats", "content": "`wca` has been updated to the latest public export! :tada:"}' $discordwh && \
+~/.mysqlpw/discord-notify.sh "\`wca\` has been updated to the latest public export! :tada:" && \
 mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW() WHERE query = 'wca'"
