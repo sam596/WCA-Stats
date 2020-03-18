828e78ae4a12354243564a1c8a0092f7db869261
sam596 <31994452+sam596@users.noreply.github.com>
2020-03-18 16:12:45 +0000

    Auto update ghpages

--- bin/wcadevstsupd.sh
+++ bin/wcadevstsupd.sh
@@ -152,11 +152,11 @@ then
 ## log that wca_stats has finished updating
   mysql -u sam -p"$mysqlpw" wca_stats -e "UPDATE last_updated SET completed = NOW(), notes = 'Change noticed; developer database imported, wca_stats updated --- (${ldu1} vs ${ldu2})' WHERE query = 'wcadevstsupd.sh'" 
 ## run ghpages stats
-#  ~/WCA-Stats/bin/ghpagesupd.sh
-## commit ghpages
-#  cd ~/pages/WCA-Stats/ && \
-#  commit=$(git log --format="%H" -n 1) && \
-#  ~/.mysqlpw/discord-notify.sh "http://sam596.github.io Updated with latest stats. \n View the changes here: https://github.com/sam596/WCA-Stats/commit/'"$commit"'. \n \n That concludes the tri-daily spam of messages thanks to the WCA updating their developer database! :smiley: See you in three days :wink: Server is now restarting"
+  ~/WCA-Stats/bin/ghpagesupd.sh
+## discord commit ghpages
+  cd ~/pages/WCA-Stats/ && \
+  commit=$(git log --format="%H" -n 1) && \
+  ~/.mysqlpw/discord-notify.sh "http://sam596.github.io Updated with latest stats. \n View the changes here: https://github.com/sam596/WCA-Stats/commit/'"$commit"'. \n \n That concludes the tri-daily spam of messages thanks to the WCA updating their developer database! :smiley: See you in three days :wink: Server is now restarting"
 ## reboot the server
   sudo reboot
  else
